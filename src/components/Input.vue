cd567b1bcaf063197c2c362b710b8f6229e4ff5d
Ker ker <kerke.2011@gmail.com>
2023-07-14 02:08:51 +0800

    exercise for refs

---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <input value="input" class="tag" @blur="$emit('toggle')" />
+</template>
+
+<style scoped>
+.tag {
+  background-color: whitesmoke;
+  border: 2px black solid;
+  padding: 10px;
+}
+
+.tag:focus {
+  border: 4px red solid;
+}
+</style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="tag" @click="$emit('toggle')">tag</div>
+</template>
+
+<style scoped>
+.tag {
+  background-color: bisque;
+  border: 2px black solid;
+}
+</style>
